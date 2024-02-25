4876c0d8461f00714551dd5518ecea612a480210
Dibash Thapa <tdiwash21@tbc.edu.np>
2024-02-25 12:56:26 +0545

    Added Background Image in our app

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,16 +4,21 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,15 +28,31 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             HappyBirthdayTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+                    Image(
+                        painterResource(id = R.drawable.androidparty),
+                        contentDescription = "abcd",
+//                      colorFilter = ColorFilter.tint(Color.Magenta)     ** It changes the color for my birthday card
+                    )
+
                     GreetingText(message = "Happy Birthday Pratik!", from = "From Dibash")
+//                    Button(onClick = { /*TODO*/ }) {
+//                        Image(
+//                            painterResource(id = R.drawable.birthdayemoji),
+//                            contentDescription = "icon",
+//                            Modifier.size(50.dp)
+//                        )
+
                 }
             }
+
         }
     }
 }
@@ -46,6 +67,7 @@ class MainActivity : ComponentActivity() {
 //}
 //@Preview(showBackground = true, name = "light mode")
 //@Preview(showBackground = true, name = "dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+
 
 
 @Composable
@@ -65,10 +87,30 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 }
 
 
-@Preview(showBackground = true)
+
+
+@Preview(showBackground = true, name = "My BirthdayWish")
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
         GreetingText(message = "Happy Birthday Pratik!", from = "From Dibash")
+
+//        for adding the image for the birthday card
+        Image(
+            painterResource(id = R.drawable.androidparty),
+            contentDescription = "abcd",
+//            colorFilter = ColorFilter.tint(Color.Magenta)     ** It changes the color for my birthday card
+        )
+        GreetingText(message = "Happy Birthday Pratik!", from = "From Dibash")
+
+//        for adding the button to your birthday application where onClick is the event
+        Button(onClick = { /*TODO*/ }) {
+            Image(
+                painterResource(id = R.drawable.birthdayemoji),
+                contentDescription = "icon",
+                Modifier.size(50.dp)
+            )
+        }
     }
+
 }
